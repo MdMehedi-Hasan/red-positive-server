47c2dbf98801e7a0430427268c10b3fc690e5fc8
MdMehedi-Hasan <mh19115@gmail.com>
2022-08-18 11:16:22 +0600

    port added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 const app = express()
-const port = 2000
+const port = process.env.PORT || 2000
 require('dotenv').config()
 
 app.use(cors())
